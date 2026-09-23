93134ed50534cf7ee5d89cbf317e7d3e7edde103
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-09-23 21:49:13 +0530

    added image reveal effect animation on mousemove

--- src/components/Para.jsx
+++ src/components/Para.jsx
@@ -1,19 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import paraImg2 from "../assets/it_img.jpg";
 import paraImg1 from "../assets/nun.jpg";
 import paraImg3 from "../assets/maxresdefault.jpg";
 
 const Para = () => {
+
+
+  // const hover=()=>{
+    
+  // }
+
+
+  const imageRef = useRef(null);
+  const dotRef = useRef(null);
+  const isActive = useRef(false);
+
+  // const
+
+  useEffect(() => {
+    gsap.set(imageRef.current, {
+      opacity: 0,
+      scale: 0,
+      clipPath: "inset(50%,50%,50%,50%)",
+    });
+
+    const handleMouse = (x) => {
+      const position = dotRef.current.getBoundingClientRect();
+
+      const X = position.left + position.width / 2;
+      const Y = position.top + position.height / 2;
+
+      const dis = Math.hypot(x.clientX - X, x.clientY - Y);
+
+      if (dis < 90 && !isActive.current) {
+        isActive.current = true;
+        gsap.killTweensOf(imageRef.current);
+        gsap.to(
+          imageRef.current,
+
+          {
+            opacity: 1,
+            scale: 1,
+            clipPath: "inset(0% ,0% ,0% ,0%)",
+            duration: 0.4,
+            ease: "power3.out",
+          },
+        );
+      }
+
+      if (dis >= 90 && isActive.current) {
+        isActive.current = false;
+
+        gsap.killTweensOf(imageRef.current);
+
+        gsap.to(imageRef.current, {
+          opacity: 0,
+          scale: 0,
+          clipPath: "inset(50%,50%,50%,50%)",
+          duration: 0.4,
+          ease: "power3.in",
+        });
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouse);
+    return () => {
+      window.removeEventListener("mousemove", handleMouse);
+    };
+  }, []);
+
   return (
-    <div className="text-white my-20 border border-white flex items-center justify-center">
+    <div className="text-white my-20  flex items-center justify-center">
       <p className="uppercase   font-livvic font-extrabold flex text-center flex-col text-7xl">
         <span>we're building</span>
         <span className="flex items-center  gap-4">
           <span className="lowercase text-[#dc2121c1] font-mouse ">a </span> new
-          night{" "}
-          <span className=" relative  inline-block h-7 w-10 rounded-lg  bg-white">
-            <span className="h-60 inline-block -right-40  w-80 absolute  -top-25 rounded-2xl overflow-hidden">
+          night
+          <span
+            ref={dotRef}
+            
+            className=" relative cursor-pointer perspective-[800px]  h-7 w-10 rounded-lg  bg-white"
+          >
+            <span
+              ref={imageRef}
+              className="h-60   -right-40  w-80 absolute  -top-25 rounded-2xl overflow-hidden"
+            >
               <img
                 className="h-full w-full object-cover "
                 src={paraImg1}
@@ -32,8 +104,8 @@ const Para = () => {
         </span>
         <span className="flex justify-center items-center gap-4">
           home{" "}
-          <span className=" relative  inline-block h-7 w-10 rounded-lg  bg-white">
-            <span className="h-60 inline-block  w-80 absolute -right-36  -top-28 rounded-2xl overflow-hidden">
+          <span ref={dotRef} className=" relative  inline-block h-7 w-10 rounded-lg  bg-white">
+            <span ref={imageRef} className="h-60   w-80 absolute -right-36  -top-28 rounded-2xl overflow-hidden">
               <img
                 className="h-full w-full object-cover "
                 src={paraImg2}
@@ -54,8 +126,8 @@ const Para = () => {
         <span className="flex items-center justify-center gap-4">
           {" "}
           un{" "}
-          <span className=" relative  inline-block h-7 w-10 rounded-lg  bg-white">
-            <span className="h-60 inline-block  w-80 absolute -left-35 -top-28 rounded-2xl overflow-hidden">
+          <span ref={dotRef} className=" relative cursor-pointer  inline-block h-7 w-10 rounded-lg  bg-white">
+            <span ref={imageRef} className="h-60 inline-block  w-80 absolute -left-35 -top-28 rounded-2xl overflow-hidden">
               <img
                 className="h-full w-full object-cover "
                 src={paraImg3}
